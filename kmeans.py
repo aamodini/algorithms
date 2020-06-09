479f12b4dcb1d0051b47d4bc32a7518dd8d697f4
aamodini <gupta.aamodini@gmail.com>
2020-06-08 23:54:58 -0500

    get iris dataset on python to use for kmeans

---
+++ kmeans.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+from sklearn.decomposition import PCA
+
+# import some data to play with
+iris = datasets.load_iris()
+iris_df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
+                     columns= iris['feature_names'] + ['target'])
+
+# always remember to wrap arounf with print otherwise there's no output
+print(iris_df.head())
