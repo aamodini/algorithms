185535e7ecf2a9b06a08f943aba0ba057eb9f3f3
aamodini <gupta.aamodini@gmail.com>
2020-06-09 23:45:51 -0500

    write out the logic behind kmeans before creating the kmeans class

--- kmeans.py
+++ kmeans.py
@@ -12,3 +12,13 @@ iris_df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
 
 # always remember to wrap arounf with print otherwise there's no output
 print(iris_df.head())
+
+# attempt to create a class for kmean
+# pseudo code for kmeans:
+# 1. Identify the number of clusters k
+# 2. Randomly split the dataset into K clusters and assign each subset a cluster label.
+# This will initialise k number of centroids at random.
+# 3. Calculate the cluster means
+# 4. Assign each observation to the closest cluster mean. So if the distance between centroidA and point1 is greater
+# then the disctance between centroidB and point1 then point1 should be reclassified to classB.
+# 5. Continue iterating through steps 3 and 4 till the centroid no longer changes.
